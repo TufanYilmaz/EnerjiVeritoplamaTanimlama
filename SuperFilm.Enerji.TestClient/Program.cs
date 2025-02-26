7cf6c39554d485960797c07e82652c59badc0c94
SANKOHOLDING\tufan.yilmaz <tufan.yilmaz@sanko.com.tr>
2025-02-26 10:16:58 +0300

    .

--- SuperFilm.Enerji.TestClient/Program.cs
+++ SuperFilm.Enerji.TestClient/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using SuperFilm.Enerji.OleDbReader;
+//using SuperFilm.Enerji.OleDbReader;
 
-Console.WriteLine("Hello, World!");
-Client reader = new Client();
-reader.ReadFile("29", DateTime.Now.AddDays(-2));
+//Console.WriteLine("Hello, World!");
+//Client reader = new Client();
+//reader.ReadFile("29", DateTime.Now.AddDays(-2));
 Console.ReadKey();
