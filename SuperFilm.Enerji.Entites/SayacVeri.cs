0a8bdcd33f8571226c6a61965f2e400d2f87857e
SANKOHOLDING\tufan.yilmaz <tufan.yilmaz@sanko.com.tr>
2025-03-04 10:44:12 +0300

    Sayacveri için class eklendi

---
+++ SuperFilm.Enerji.Entites/SayacVeri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFilm.Enerji.Entites
+{
+    [Table("SAYAC_VERI")]
+    public class SayacVeri
+    {
+        [Key]
+        public int Id { get; set; }
+
+    }
+}
