c49c0787ab70fcdc7e04ca8aed7affc6f68e2a7a
SANKOHOLDING\tufan.yilmaz <tufan.yilmaz@sanko.com.tr>
2025-03-05 10:32:56 +0300

    Okuma için clinentler eklendi

--- SuperFilm.Enerji.CollectData/Program.cs
+++ SuperFilm.Enerji.CollectData/Program.cs
@@ -1,7 +1,10 @@
 using SuperFilm.Enerji.CollectData;
+using SuperFilm.Enerji.Entites;
+using TanvirArjel.EFCore.GenericRepository;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
-
+builder.Services.AddGenericRepository<EnerjiDbContext>();
+builder.Services.AddQueryRepository<EnerjiDbContext>();
 var host = builder.Build();
 host.Run();
