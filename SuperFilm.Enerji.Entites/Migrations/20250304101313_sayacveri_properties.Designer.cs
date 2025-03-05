07afcfa01d759050140ed34e5a46c014312e4108
SANKOHOLDING\tufan.yilmaz <tufan.yilmaz@sanko.com.tr>
2025-03-05 08:27:04 +0300

    Sayac Verileri için tablo entity eklendi

--- SuperFilm.Enerji.Entites/Migrations/20250304101313_sayacveri_properties.Designer.cs
+++ SuperFilm.Enerji.Entites/Migrations/20250304101313_sayacveri_properties.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SuperFilm.Enerji.Entites;
 
@@ -11,9 +12,11 @@ using SuperFilm.Enerji.Entites;
 namespace SuperFilm.Enerji.Entites.Migrations
 {
     [DbContext(typeof(EnerjiDbContext))]
-    partial class EnerjiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250304101313_sayacveri_properties")]
+    partial class sayacveri_properties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SuperFilm.Enerji.Entites/SayacVeri.cs
+++ SuperFilm.Enerji.Entites/SayacVeri.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,24 @@ namespace SuperFilm.Enerji.Entites
     {
         [Key]
         public int Id { get; set; }
+        [Column(TypeName = "varchar(4)")]
+        [StringLength(4)]
+        [Required]
+        public string Yil { get; set; }
+        [Column(TypeName = "varchar(2)")]
+        [StringLength(2)]
+        [Required]
+        public string Ay { get; set; }
+        [Column(TypeName = "varchar(6)")]
+        [StringLength(6)]
+        [Required]
+        public string Zaman { get; set; }
+        [Required]
+        public DateTime NormalizeDate { get; set; }
+        [Required]
+        public decimal Deger { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
 
+        public SayacTanimlari Sayac { get; set; }
     }
 }
