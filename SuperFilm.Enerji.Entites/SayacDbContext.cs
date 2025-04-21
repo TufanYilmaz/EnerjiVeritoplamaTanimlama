f91545c02149ffb396016b668355a6b7f4ab687d
Kerem Emir Ercan <kerememirercan@gmail.com>
2025-04-21 15:35:59 +0300

    Client eklendi

---
+++ SuperFilm.Enerji.Entites/SayacDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperFilm.Enerji.Entites
+{
+    public class SayacDbContext : DbContext
+    {
+        public DbSet<SayacVeri> SayacVerileri { get; set; }
+        public DbSet<SayacTanimlari> sayaclar { get; set; }
+
+        public SayacDbContext(DbContextOptions<SayacDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
