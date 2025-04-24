8f52dbd37d60288776b402084935164e80b44fb0
Kerem Emir Ercan <kerememirercan@gmail.com>
2025-04-24 16:19:17 +0300

    SayacVeriFiltreleme_Eklendi

---
+++ SuperFilm.Enerji.WebUI/Controllers/SayacVeriController.cs
@@ -0,0 +1,113 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperFilm.Enerji.Entites;
+using SuperFilm.Enerji.WebUI.ViewModels;
+using TanvirArjel.EFCore.GenericRepository;
+
+namespace SuperFilm.Enerji.WebUI.Controllers
+{
+    public class SayacVeriController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRepository _repository;
+        private readonly IQueryRepository<EnerjiDbContext> _queryRepository;
+        private readonly IRepository<EnerjiDbContext> _enerjiRepository;
+
+        public SayacVeriController(ILogger<HomeController> logger,
+            IRepository repository,
+            IQueryRepository<EnerjiDbContext> queryRepository,
+            IRepository<EnerjiDbContext> enerjiRepository)
+        {
+            _logger = logger;
+            _repository = repository;
+            _queryRepository = queryRepository;
+            _enerjiRepository = enerjiRepository;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public async Task<IActionResult> FilterSayacVeri()
+        {
+            
+            var sayaclar = await _queryRepository.GetQueryable<SayacTanimlari>().ToListAsync();
+            var opcnodeslar = await _queryRepository.GetQueryable<OpcNodes>().ToListAsync();
+
+            var model = new RequestViewModel
+            {
+                SayacTanimlari = sayaclar,
+                OpcNodes = opcnodeslar,
+                SayacVeri = new List<SayacVeri>(),
+                SayacVeriRequest = new SayacVeriRequest()
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> FilterSayacVeri(SayacVeriRequest sayacVeriRequest)
+        {
+            var sayaclar = await _queryRepository.GetQueryable<SayacTanimlari>().ToListAsync();
+            var opcnodeslar = await _queryRepository.GetQueryable<OpcNodes>().ToListAsync();
+            List<SayacVeri> sayacverileri = [];
+
+            if (ModelState.ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+            {
+                var m = new RequestViewModel
+                {
+                    SayacTanimlari = sayaclar,
+                    OpcNodes = opcnodeslar,
+                    SayacVeri = sayacverileri,
+                    SayacVeriRequest = new SayacVeriRequest()
+                };
+                return View(m);
+            }
+            var sayacverileriQuery = _queryRepository.GetQueryable<SayacVeri>()
+                .Where(r=> sayacVeriRequest.DataTypeId==1 ? r.SayacId>0 : r.OpcNodesId>0 );
+
+            if (sayacVeriRequest.DataTypeId == 1 && sayacVeriRequest.SayacOpcNodesID > 0)
+            {
+                sayacverileriQuery= sayacverileriQuery.Where(i => i.SayacId == sayacVeriRequest.SayacOpcNodesID);
+            }
+            if (sayacVeriRequest.DataTypeId == 2 && sayacVeriRequest.SayacOpcNodesID > 0)
+            {
+                sayacverileriQuery = sayacverileriQuery.Where(i => i.OpcNodesId == sayacVeriRequest.SayacOpcNodesID);
+            }
+            if (sayacVeriRequest.StartDate !=null && sayacVeriRequest.StartDate != default)
+            {
+                sayacverileriQuery = sayacverileriQuery.Where(i => i.NormalizeDate > sayacVeriRequest.StartDate);
+            }
+            if (sayacVeriRequest.EndDate != null &&  sayacVeriRequest.EndDate != default)
+            {
+                sayacverileriQuery = sayacverileriQuery.Where(i => i.NormalizeDate < sayacVeriRequest.EndDate);
+            }
+            if (sayacVeriRequest.OrderBy == OrderBy.Ascending)
+            {
+                sayacverileriQuery = sayacverileriQuery.OrderBy(i => i.NormalizeDate);
+            }
+            if (sayacVeriRequest.OrderBy == OrderBy.Descending)
+            {
+                sayacverileriQuery = sayacverileriQuery.OrderByDescending(i => i.NormalizeDate);
+            }
+
+            sayacverileri = await sayacverileriQuery
+                .Take(sayacVeriRequest.NumData)
+                .ToListAsync();
+
+            var model = new RequestViewModel
+            {
+                SayacTanimlari = sayaclar,
+                OpcNodes = opcnodeslar,
+                SayacVeri = sayacverileri,
+                SayacVeriRequest = sayacVeriRequest
+            };
+            return View(model);
+        }
+        public IActionResult ListSayacVeri()
+        {
+            return View();
+        }
+    }
+}
--- SuperFilm.Enerji.WebUI/wwwroot/js/site.js
+++ SuperFilm.Enerji.WebUI/wwwroot/js/site.js
@@ -1,9 +1,26 @@
 ﻿
-$(document).ready(function () {
-	$(".pageTable").DataTable({
-		language: {
-			url: '//cdn.datatables.net/plug-ins/2.2.2/i18n/tr.json',
-		},
-	});
+//$(document).ready(function () {
+//	$(".pageTable").DataTable({
+//		language: {
+//			url: '//cdn.datatables.net/plug-ins/2.2.2/i18n/tr.json',
+//		},
+//	});
 
+//});
+
+$(document).ready(function () {
+    const $table = $("#pageTable");
+
+    const hasRealData = $table.find("tbody tr").filter(function () {
+        const td = $(this).find("td");
+        return !(td.length === 1 && td.attr("colspan")); // sadece bilgi satırı değilse
+    }).length > 0;
+
+    if (hasRealData) {
+        $table.DataTable({
+            language: {
+                url: '//cdn.datatables.net/plug-ins/2.2.2/i18n/tr.json',
+            },
+        });
+    }
 });
